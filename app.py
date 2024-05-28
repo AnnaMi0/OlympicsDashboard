c9e2cdc3eb4483a02a82ffae791458227acaf6b9
Anna <annamixailidou04@gmail.com>
2024-05-29 02:11:36 +0300

    optimised code

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_data_from_csv():
         "athlete_events.csv",
         header=0,
         usecols=lambda column: column != "A",
-        nrows=1000
+
     )
     return df
 
@@ -24,37 +24,21 @@ print(df.columns)
 st.dataframe(df)
 
 #-------SIDEBAR-------
-st.sidebar.header("Filter Here:")
+def create_multiselect_filter(title, column, df):
+    options = df[column].dropna().unique()
+    if column == "Team" or column == "Sport" or column == "Event":
+        options = sorted(options, key=lambda x: str(x))
+    return st.sidebar.multiselect(
+        title,
+        options=options,
+        default=list(options)
+    )
 
-Team = st.sidebar.multiselect(
-    "Select Team:",
-    options=df["Team"].unique(),
-    default=list(df["Team"].unique())
-)
-
-Age = st.sidebar.multiselect(
-    "Select Age:",
-    options=df["Age"].unique(),
-    default=list(df["Age"].dropna().unique()) # dropna to handle missing values in 'Medal'
-)
-
-Sport = st.sidebar.multiselect(
-    "Select Sport:",
-    options=df["Sport"].unique(),
-    default=list(df["Sport"].unique())
-)
-
-Event = st.sidebar.multiselect(
-    "Select Event:",
-    options=df["Event"].unique(),
-    default=list(df["Event"].unique())
-)
-
-Medal = st.sidebar.multiselect(
-    "Select Medal:",
-    options=df["Medal"].dropna().unique(),  
-    default=list(df["Medal"].dropna().unique())
-)
+Team = create_multiselect_filter("Select Team:", "Team", df)
+Age = create_multiselect_filter("Select Age:", "Age", df)
+Sport = create_multiselect_filter("Select Sport:", "Sport", df)
+Event = create_multiselect_filter("Select Event:", "Event", df)
+Medal = create_multiselect_filter("Select Medal:", "Medal", df)
 
 # Filtering dataframe based on selections
 df_selection = df.query(
