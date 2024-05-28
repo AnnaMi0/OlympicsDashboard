a1ae46b76640899782927a779bcf2e75e4b3e318
Anna <annamixailidou04@gmail.com>
2024-05-29 02:43:22 +0300

    improved interface

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ st.set_page_config(page_title="Olympics Dashboard",
                    layout="wide"
                    )
 
-
 @st.cache_data
-
 def get_data_from_csv():
     df = pd.read_csv(
         "athlete_events.csv",
         header=0,
         usecols=lambda column: column != "A",
-
     )
+    # Data Cleaning
+    df['Age'] = pd.to_numeric(df['Age'], errors='coerce')  # Ensure 'Age' is numeric, coerce errors to NaN
+    df = df.dropna(subset=['Age'])  # Drop rows where 'Age' is NaN
     return df
 
 df = get_data_from_csv()
@@ -24,25 +24,41 @@ print(df.columns)
 st.dataframe(df)
 
 #-------SIDEBAR-------
+# Dropdown for single selection
+def create_selectbox_filter(title, column, df):
+    options = df[column].dropna().unique()
+    if column in ["NOC", "Sport", "Event"]:
+        options = sorted(options, key=lambda x: str(x))
+    return st.sidebar.selectbox(
+        title,
+        options=options,
+        index=0  # You can set this to -1 to have nothing selected initially, but Streamlit selectbox does not support no selection.
+    )
+
+# Multiselect for multiple selections, default to no selections
 def create_multiselect_filter(title, column, df):
     options = df[column].dropna().unique()
-    if column == "Team" or column == "Sport" or column == "Event":
+    if column in ["NOC", "Sport", "Event"]:
         options = sorted(options, key=lambda x: str(x))
     return st.sidebar.multiselect(
         title,
         options=options,
-        default=list(options)
+        default=[]  # All options deselected by default
     )
 
-Team = create_multiselect_filter("Select Team:", "Team", df)
-Age = create_multiselect_filter("Select Age:", "Age", df)
+NOC = create_multiselect_filter("Select NOC:", "NOC", df)
+Age = st.sidebar.slider("Select Age Range:", int(df['Age'].min()), int(df['Age'].max()), (int(df['Age'].min()), int(df['Age'].max())))
 Sport = create_multiselect_filter("Select Sport:", "Sport", df)
 Event = create_multiselect_filter("Select Event:", "Event", df)
 Medal = create_multiselect_filter("Select Medal:", "Medal", df)
 
 # Filtering dataframe based on selections
-df_selection = df.query(
-    "Team == @Team & Age == @Age & Sport == @Sport & Event == @Event & Medal == @Medal"
-)
+df_selection = df[
+    (df['NOC'].isin(NOC) if NOC else True) &
+    (df['Age'] >= Age[0]) & (df['Age'] <= Age[1]) &
+    (df['Sport'].isin(Sport) if Sport else True) &
+    (df['Event'].isin(Event) if Event else True) &
+    (df['Medal'].isin(Medal) if Medal else True)
+]
 
 st.dataframe(df_selection)
